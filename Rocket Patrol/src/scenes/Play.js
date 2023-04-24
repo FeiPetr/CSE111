740ae58bec2d9e8328cd4ba5e05450387fcda4d5
FeiPetr <777spetrova@gmail.com>
2023-04-24 12:41:17 -0700

    Adding menu and play

---
+++ Rocket Patrol/src/scenes/Play.js
@@ -0,0 +1,10 @@
+class Play extends Phaser.Scene{ //creating js class 'menu' that extends phaser's predef scene object
+    constructor() // The constructor (a special method for creating and initializing an object) uses
+    {             // the "super" keyword to call the constructor of the super class
+        super("playScene");
+    }
+
+    create(){
+        this.add.text(20,20,"Rocket Patrol Menu");
+    }
+}
