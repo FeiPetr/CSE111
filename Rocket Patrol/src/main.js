756d69eae1b4a0d54742b7b8017f50d1d6374653
FeiPetr <777spetrova@gmail.com>
2023-04-24 15:19:56 -0700

    Fixing bug?

--- Rocket Patrol/src/main.js
+++ Rocket Patrol/src/main.js
@@ -5,3 +5,7 @@ let config = {
     scene: [Menu,Play]
 }
 let game = new Phaser.Game(config); //initialize, config canvas size
+//Set UI sizes, border+padding
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
+
