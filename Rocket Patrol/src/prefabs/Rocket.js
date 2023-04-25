9000d8cff7337f721295e5de7dbe62e44f2b938d
FeiPetr <777spetrova@gmail.com>
2023-04-24 18:52:46 -0700

    adding key movement

--- Rocket Patrol/src/prefabs/Rocket.js
+++ Rocket Patrol/src/prefabs/Rocket.js
@@ -5,6 +5,37 @@ class Rocket extends Phaser.GameObjects.Sprite {
   
       // add object to existing scene
       scene.add.existing(this);
+      this.isFiring = false; //track rocket's firing status
+      this.moveSpeed = 2; //pixels per frame
+    }
+
+    update()
+    {
+      if(!this.isFiring){
+        if(keyLeft.isDown && this.x >= borderUISize + this.width)
+        {
+          this.x -= this.moveSpeed;
+        } else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width)
+        {
+          this.x += this.moveSpeed;
+        }
+      }
+      //fire button
+      if(Phase.Input.Keyboard.JustDown(keyF)){
+        this.isFiring = true;
+      }
+      //if fired, move up
+      if(this.isFiring && this.y >= borderUISize * 3 + borderPadding)
+      {
+        this.y -= this.moveSpeed;
+      }
+      //reset on miss
+      if(this.y <= borderUISize * 3 +borderPadding){
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
+      }
+      this.p1Rocket.update();
+
     }
   }
   
